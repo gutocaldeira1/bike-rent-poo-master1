263dc9a266bec580f9a9ccaee34246a4436200da
jose augusto <caldeira.guto@gmail.com>
2023-10-09 00:43:48 -0300

    aa

---
+++ src/crypt.ts
@@ -0,0 +1,13 @@
+import  bcrypt  from '../node_modules/@types/bcrypt/index'
+
+export class Crypt {
+    private rounds = 10
+
+    async encrypt(plain: string): Promise<string> {
+        return await bcrypt.hash(plain, this.rounds)
+    }
+
+    async compare(plain: string, encrypted: string): Promise<boolean> {
+        return await bcrypt.compare(plain, encrypted)
+    }
+}
